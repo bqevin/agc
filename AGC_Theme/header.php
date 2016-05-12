d2a0ac92c15af3386151bddb0ff5bfb0e10f6099
Kevin Barasa <bkevin001@yahoo.com>
2016-05-12 03:25:19 +0300

    Changes inflicted

---
+++ AGC_Theme/header.php
@@ -0,0 +1,71 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+Design : Kevin Barasa | kevin.barasa001@gmail.com
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Africa Centre for Gender, Social Research and Impact Assessment | Welcome</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="<?php echo get_bloginfo('template_url') ?>/css/custom.css" rel="stylesheet" type="text/css" media="all"/>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700,500,900' rel='stylesheet' type='text/css'>
+<script src="<?php echo get_bloginfo('template_url') ?>/js/jquery.min.js" type="text/javascript"></script>
+<script src="<?php echo get_bloginfo('template_url') ?>/js/script.js" type="text/javascript"></script>
+<script src="<?php echo get_bloginfo('template_url') ?>/js/jquery.magnific-popup.js" type="text/javascript"></script>
+<link href="<?php echo get_bloginfo('template_url') ?>/css/magnific-popup.css" rel="stylesheet" type="text/css">
+<script src="<?php echo get_bloginfo('template_url') ?>/js/materialize.js" type="text/javascript"></script>
+<link href="<?php echo get_bloginfo('template_url') ?>/css/materialize.css" rel="stylesheet" type="text/css">
+<!-- Add fancyBox main JS and CSS files -->
+	<script type="text/javascript" src="<?php echo get_bloginfo('template_url') ?>/js/jquery.fancybox.js"></script>
+	<link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('template_url') ?>/css/jquery.fancybox.css" media="screen" />
+	<script type="text/javascript" src="<?php echo get_bloginfo('template_url') ?>/js/jquery.fancybox-media.js"></script>	
+</head>
+<body>
+  <div class="header" id="home">
+  	  <div class="header_top">
+	   <div class="wrap">
+		 	     <div class="logo">
+						<a href="index.php"><img src="<?php echo get_bloginfo('template_url') ?>/images/logo.png" alt="" /></a>
+					</div>	
+						<div class="menu">
+						    <ul>
+						    	<li><a href="index.php" class="scroll">Home</a></li>
+								<li><a href="#features" class="scroll">What We Do</a></li>
+								<li><a href="#dashboard" class="scroll">Reports</a></li>
+								<li><a href="#customers" class="scroll">Readers Digest</a></li>	
+<li><a href="contact-us.php">Get in Touch</a></li>
+								<!-- <li><a class="popup-with-zoom-anim" href="#small-dialog">Pop up</a></li>	 -->	 			    
+								<!-- <li class="login" >
+									<div id="loginContainer"><a href="#" id="loginButton"><span>Login</span></a>
+						                <div id="loginBox">                
+						                    <form id="loginForm">
+						                        <fieldset id="body">
+						                            <fieldset>
+						                                <label for="email">Email Address</label>
+						                                <input type="text" name="email" id="email">
+						                            </fieldset>
+						                            <fieldset>
+						                                <label for="password">Password</label>
+						                                <input type="password" name="password" id="password">
+						                            </fieldset>
+						                            <input type="submit" id="login" value="Sign in">
+						                            <label for="checkbox"><input type="checkbox" id="checkbox"> <i>Remember me</i></label>
+						                        </fieldset>
+						                        <span><a href="#">Forgot your password?</a></span>
+						                    </form>
+						                </div>
+						               </div>
+								   </li> -->
+								<div class="clear"></div>
+							</ul>
+						</div>							
+	    		 <div class="clear"></div>
+	        </div>
+	    </div>
+	 </div>		
---
+++ AGC_Theme/index.php
@@ -0,0 +1,203 @@
+<!--Header Partial-->	
+<?php get_header(); ?>
+<!--Body Partial-->	      	
+     <div class="main">
+	 	<div class="content">	
+	 		 <div class="content_top">  
+	 		     <div class="wrap">                                  		
+            	   <div class="banner_desc">
+            		<h1>Africa <span>Gender</span> Center </h1>  
+            		<h3><i>Providing consultancy company on Gender, Social <br /> Research and Impact Assessment</i></h3>
+            		<a class="play_icon fancybox-media" href="#"><img src="<?php bloginfo('template_directory'); ?>/images/play-icon.png" alt="" /></a>
+            		<h3>Get our reasearches <span>inboxed</span>!</h3>
+            		<p>No spamming. We promise</p>
+            		 <div class="sign_up">				  	
+					 <form>
+					  		<input style="padding: 3px 2px 1px;" type="text" value="E-mail address " onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'E-mail address';}"> 
+					 		<input type="submit" value="Subscribe" style="padding: 11px 20px;" >
+					  </form>
+				    </div>
+                </div>  
+                     <div class="ipad">
+            		     <img src="<?php bloginfo('template_directory'); ?>/images/ipad.png" alt="" />
+            	     </div>
+             <div class="clear"></div>
+        </div>
+     </div>
+           <div class="features" id="features">
+                 <div class="wrap">                             	
+                 		  <h2>What We Do at <span>Africa Gender Center</span></h2>
+                 		    <h4>find out of our core activities</h4>
+                 		        <div class="features_grids">
+							     <div class="section group">
+									<div class="grid_1_of_4 images_1_of_4">
+										 <img src="<?php bloginfo('template_directory'); ?>/images/gender.png" alt="" />
+										 <h3>Gender Analysis and Training</h3>
+										 <p>We have a roster of consultants with experience in gender analysis in agriculture, general and specialised gender training</p>
+									</div>
+									<div class="grid_1_of_4 images_1_of_4">
+										 <img src="<?php bloginfo('template_directory'); ?>/images/eval.png" alt="" />
+										 <h3>Evaluation Studies</h3>
+										 <p>We conduct mid term and end of project evaluation studies focused on analysing the social and economic impacts of agriculture research and development programs</p>
+									</div>
+									<div class="grid_1_of_4 images_1_of_4">
+										 <img src="<?php bloginfo('template_directory'); ?>/images/projects.png" alt="" />
+										  <h3>Integrating Gender in Projects and Programs</h3>
+										  <p>This is essential not only for achieving project goals but also for achieving gender equality. Our consultants work with you to ensure gender is integral in your programming</p>
+									</div>
+									<div class="grid_1_of_4 images_1_of_4">
+										 <img src="<?php bloginfo('template_directory'); ?>/images/publishing.png" alt="" />
+										  <h3>Publishing and Information Sharing</h3>
+										  <p>ACGSRIA publishes the Journal of Gender, Agriculture and Food Security. For more information on the <a target="_blank" href="http://www.agrigender.net">journal</a> Visit our blog at www.agrigenderjournal.com</p>
+									</div>
+					              </div>
+					              <div class="button"><a class="popup-with-zoom-anim" href="#small-dialog">See all features <img src="<?php bloginfo('template_directory'); ?>/images/arrow.png" alt="" /></a>
+					              	
+					              	<div id="small-dialog" class="mfp-hide">
+					                           	    <div class="plans_table">
+  			 	
+								  			 	<table width="100%" cellspacing="0" class="compare_plan">
+												<thead>
+								   					<tr>
+								        				<th class="plans-list"><h3>Project Name</h3></th>
+								        				<th class="plans-list"><h3>Where</h3><h4><small>(Location : Nairobi, Kenya)</small></h4></th>
+								        				<th class="plans-list"><h3>Time</h3><h4><small>June - November</small></h4></th>
+								    				</tr>
+												</thead>
+													<tfoot>
+									    			<tr>
+												        <td> </td>
+								        				<td class="order_now"><a href="#account" class="scroll"><button>Order Now</button></a></td>
+								        				<td class="order_now"><a href="#account" class="scroll"><button>Order Now</button></a></td>
+													</tr>
+													</tfoot>				
+								   				<tbody>
+								   					<tr>
+									        			<td class="plan_list_title">Web Space</td>
+												        <td class="price_body">Unlimited</td>
+												        <td class="price_body">Unlimited</td>
+									    			</tr>
+								    				<tr>
+								        				<td class="plan_list_title">Bandwidth</td>
+														<td class="price_body">Unlimited</td>
+										        		<td class="price_body">Unlimited</td>
+								    				</tr>
+								    		
+								    				<tr>
+								        				<td class="plan_list_title">Wordpress Support</td>
+												        <td class="price_body"><img src="<?php bloginfo('template_directory'); ?>/images/icon-remove.png" alt="img"></td>
+											    	    <td class="price_body"><img src="<?php bloginfo('template_directory'); ?>/images/tickmark-icon.png" alt="img"></td>
+								    				</tr>
+								    		
+								    				<tr>
+								        				<td class="plan_list_title">Dedicated IP</td>
+												        <td class="price_body">$2/mo</td>
+												        <td class="price_body">$2/mo</td>
+								    				</tr>
+												</tbody></table>  			 	
+  											 </div>
+												</div>
+											   <script>
+													$(document).ready(function() {
+														$('.popup-with-zoom-anim').magnificPopup({
+															type: 'inline',
+													
+															fixedContentPos: false,
+															fixedBgPos: true,
+													
+															overflowY: 'auto',
+													
+															closeBtnInside: true,
+															preloader: false,
+															
+															midClick: true,
+															removalDelay: 300,
+															mainClass: 'my-mfp-zoom-in'
+														});
+																					
+													});
+													</script>
+					              	
+					              </div>
+					        </div>
+						</div>
+           			</div>
+           			 
+           	  <div class="dashborder_img" id="dashboard">
+               <div class="wrap">                             	
+             		  <h2>Powerful new <span>findings</span></h2>
+             		    <h4>Comprehensive latest sex-disaggregated data and gender statistics</h4>
+             		      <image src="<?php bloginfo('template_directory'); ?>/images/dashboard.jpg" alt="" />  
+			   </div>
+       		  </div>
+          <div class="content_bottom" id="customers">
+          <div class="wrap">
+          	 <h2>Readers <span>Digest</span></h2>
+                 		    <h4>Here is what our writers have mind</h4>
+          </div>
+          	  <div class="row">
+          	  <?php query_posts('posts_per_page=8&cat=1'); /*1, 2*/
+				 if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+          	  <div class="col s12 m6 l3">
+			    	<div class="card small">
+					    <div class="card-image waves-effect waves-block waves-light" style="z-index:0;">
+						<?php echo get_the_post_thumbnail( $page->ID, 'post_thumbnail', 'style=max-width:100%;z-index:0;height:auto;' ); ?>
+					      
+					    </div>
+					    <div class="card-content">
+					      <span class="card-title activator grey-text text-darken-4" ><?php the_title();?><i class="material-icons right">more_vert</i></span>
+					      <p>Readmore</p>
+					    </div>
+					    <div class="card-reveal">
+					      <span class="card-title grey-text text-darken-4" style="font-size: 17px; "><?php the_title(); ?><i class="material-icons right">close</i></span>
+					      <br>
+					      <a href="<?php the_permalink(); ?>">
+					      <small> <?php the_excerpt(); ?></small>
+					      </a>
+					    </div>
+
+					  </div> 
+			    </div>
+			    <?php endwhile; ?> <?php wp_reset_query(); /*4*/ ?>
+          </div>    
+          <div class="content_bottom" id="customers">
+              <div class="wrap">            
+                 		  <h2>Highest Customer <span>Satisfaction</span></h2>
+                 		    <h4>Our success is measured through the success of our customers.</h4>
+                 		        <!-- <div class="customers_grids">
+							       <div class="section group">
+									  <div class="grid_1_of_3 images_1_of_3">
+									  	 <div class="customer_desc">
+									  	 	<span class="quotes up"> </span><p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p><span class="quotes down"> </span> 
+										   <span class="testimonial-arrow"> </span>
+										 </div> 
+										  <img src="<?php bloginfo('template_directory'); ?>/images/customer-img1.jpg" alt="" />
+										  <h3><a href="#">Uku Mason</a></h3>
+										  <h5>Marketing Expert</h5>
+									</div>
+									<div class="grid_1_of_3 images_1_of_3">
+										<div class="customer_desc">
+									  	 	<span class="quotes up"> </span><p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p><span class="quotes down"> </span>
+										  <span class="testimonial-arrow"> </span>
+										 </div> 
+										    <img src="<?php bloginfo('template_directory'); ?>/images/customer-img2.jpg" alt="" />
+										    <h3><a href="#">Ro Kanth</a></h3>
+										  <h5>Photographer</h5>
+									</div>
+									<div class="grid_1_of_3 images_1_of_3">
+										 <div class="customer_desc">
+									  	 	<span class="quotes up"> </span><p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p><span class="quotes down"> </span>
+										    <span class="testimonial-arrow"> </span>
+										 </div> 
+										    <img src="<?php bloginfo('template_directory'); ?>/images/customer-img3.jpg" alt="" />
+										    <h3><a href="#">Anna Alexander</a></h3>
+										    <h5>Professional Model</h5>
+									</div>
+								</div>
+					        </div> -->
+           			  </div>             
+       			</div>
+     		</div>
+  		 </div>	
+<!--Footer Partial-->	
+<?php get_footer(); ?>
